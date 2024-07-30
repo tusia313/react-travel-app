ad5edff7614cff3fc7d67be422b1fd86bea99953
Marta Łoskot <loskotmarta@gmail.com>
2024-07-30 20:11:34 +0200

    Working on the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import { Routes, Route, BrowserRoute  } from 'react-router-dom'
+import { Routes, Route, BrowserRouter  } from 'react-router-dom'
 import Post from './pages/Post'
 import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
     <div className="app">
-      <BrowserRoute>
+      <BrowserRouter>
       <Routes>
-        <Route path={'/'} example={<Dashboard/>}/>
-        <Route path={'/posts/:id'} example={<Post/>}/>
+        <Route path={'/'} element={<Dashboard/>}/>
+        <Route path={'/posts/:id'} element={<Post/>}/>
       </Routes>
-      </BrowserRoute>
+      </BrowserRouter>
     </div>
   )
 }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,16 @@
 const Dashboard = () => {
-    return(
-        <div>dashboard</div>
+    return (
+        <div className="dashboard">
+            <div className="dashboard-info-container">
+                <div>
+                    <h1>Adventure anywhere</h1>
+                    <p>Keep calm && travel on</p>
+                </div>
+                <button>add Your adventure</button>
+            </div>
+
+            <div className="posts-container"></div>
+        </div>
     )
 }
 export default Dashboard
