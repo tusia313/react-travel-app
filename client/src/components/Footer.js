f65b25f4763251d72ec5101f85482df122ed5091
Marta Łoskot <loskotmarta@gmail.com>
2024-07-31 20:43:27 +0200

    Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,11 @@
+import logo from '../images/logo_2.png'
+
 const Footer = () => {
-    return(
+    return (
         <footer>
-            FOOTER
+            <div className="footer-container">
+                <img className="logo" src={logo} alt="ready_to_fly logo" />
+            </div>
         </footer>
     )
 }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,12 +7,20 @@ const Dashboard = () => {
                         <h1>Adventure anywhere</h1>
                         <p>keep calm & travel on</p>
                     </div>
-                    <button>Add adventure</button>
+                    <button class="cta">
+                        <span class="hover-underline-animation"> + Add adventure</span>
+                        <path
+                            id="Path_10"
+                            data-name="Path 10"
+                            d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+                            transform="translate(30)"
+                        ></path>
+                    </button>
                 </div>
 
                 <div className="posts-container"></div>
             </div>
-        </div>
+        </div >
     )
 }
 export default Dashboard
