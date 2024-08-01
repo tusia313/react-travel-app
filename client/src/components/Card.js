54b779139d3334cfb87b70a374238ad8620b2cd4
Marta Łoskot <loskotmarta@gmail.com>
2024-08-01 18:41:29 +0200

    Card components

---
+++ client/src/components/Card.js
@@ -0,0 +1,16 @@
+const Card = () => {
+    return(
+        <div className="card-container">
+            <div className="image-container">
+                <img src={} alt={} />
+            </div>
+            <div className="info-container">
+                <h3>title</h3>
+                <p>description</p>
+                <div className="tag-container"></div>
+            </div>
+
+        </div>
+    )
+}
+export default Card
