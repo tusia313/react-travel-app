6a5ece16e57571f8cfa33f5e4fb712648e483fea
Marta Łoskot <loskotmarta@gmail.com>
2024-08-26 12:40:10 +0200

    Correction of the button

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -11,19 +11,12 @@ const Dashboard = () => {
         <div className="app">
             <div className="dashboard">
                 <div className="dashboard-info-container">
-                    <div>
-                        <h1>Adventure anywhere</h1>
+                    <h1>Adventure anywhere</h1>
+                    <div className="dashboard-button-container">
                         <p>keep calm & travel on</p>
+                        <button>+ add adventure</button>
                     </div>
-                    <button class="cta">
-                        <span class="hover-underline-animation"> + add <br/> adventure</span>
-                        <path
-                            id="Path_10"
-                            data-name="Path 10"
-                            d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
-                            transform="translate(30)"
-                        ></path>
-                    </button>
+
                 </div>
 
                 <div className="posts-container"></div>
