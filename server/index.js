099fe19fb9ee52439ad1f8465e20fe5b1e9a8c49
Marta Łoskot <loskotmarta@gmail.com>
2024-08-31 17:36:51 +0200

    Getting data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,33 @@
 PORT = 8000
+const { default: axios } = require('axios')
 const express = require('express')
+require('dotenv').config()
 const app = express()
+
 app.listen(PORT, console.log('Server is listening on port ' + PORT))
+
+// Default
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome to my travel app !')
+})
+
+// Get all posts
+app.get('/posts', async (req, res) => {
+    const url = `${process.env.ASTRA_URL}?page-size=20`
+
+    const options = {
+        method: "GET",
+        headers: {
+            'X-Cassandra-Token': process.env.TOKEN
+        }
+    }
+
+    try {
+        const response = await axios(url, options)
+        res.status(200).json(response.data)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message : err })
+    }
+})
+
