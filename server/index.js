2b989ddc2749010a325a45e243dec2eb8c54da3e
Marta Łoskot <loskotmarta@gmail.com>
2024-09-04 16:25:57 +0200

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 PORT = 8000
 const { default: axios } = require('axios')
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 const app = express()
+
+app.use(cors())
 
 app.listen(PORT, console.log('Server is listening on port ' + PORT))
 
