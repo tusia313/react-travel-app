023cb26c94ddda28b08b6605e071b7904f14523a
Marta Łoskot <loskotmarta@gmail.com>
2025-04-29 17:31:55 +0200

    Common components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Post from './pages/Post'
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <>
     {/* this syntax is how you create pages in react project */}
       <BrowserRouter>
+      <Header></Header>
         <Routes>
           <Route path={'/'} element={<Dashboard/>}></Route>
           <Route path={'/posts/:id'} element={<Post/>}></Route>
         </Routes>
+        <Footer></Footer>
       </BrowserRouter>
     </>
   )
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,14 +1,16 @@
 const Dashboard = () => {
     return (
-        <div className = "dashboard">
-            <div className = "dashboard-info-container">
-                <div>
-                    <h1>Adventure anywhere</h1>
-                    <p>Keep calm && travel on</p>
+        <div className="app">
+            <div className="dashboard">
+                <div className="dashboard-info-container">
+                    <div>
+                        <h1>Adventure anywhere</h1>
+                        <p>Keep calm && travel on</p>
+                    </div>
+                    <button>Add Your adventure</button>
                 </div>
-                <button>Add Your adventure</button>
+                <div className="posts-container"></div>
             </div>
-            <div className = "posts-container"></div>
         </div>
     )
 }
