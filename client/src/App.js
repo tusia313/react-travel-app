3616b4bbac74bf51468fdfa80a71ef2c90cbac80
Marta Łoskot <loskotmarta@gmail.com>
2025-04-29 14:29:38 +0200

    Creating pages in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Post from './pages/Post'
 const App = () => {
   return (
-    <div className="app">
-    </div>
+    <>
+    {/* this syntax is how you create pages in react project */}
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Dashboard/>}></Route>
+          <Route path={'/posts/:id'} element={<Post/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
