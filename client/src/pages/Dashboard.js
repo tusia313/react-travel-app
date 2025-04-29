c1e8f0cdbdaa90454dd77b10f3b189d7070145f4
Marta Łoskot <loskotmarta@gmail.com>
2025-04-29 17:05:53 +0200

    Working on the dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,15 @@
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className = "dashboard">
+            <div className = "dashboard-info-container">
+                <div>
+                    <h1>Adventure anywhere</h1>
+                    <p>Keep calm && travel on</p>
+                </div>
+                <button>Add Your adventure</button>
+            </div>
+            <div className = "posts-container"></div>
+        </div>
     )
 }
 
