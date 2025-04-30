226d385a1b89c8d601257e43a6e4952ba66a2483
Marta Łoskot <loskotmarta@gmail.com>
2025-04-30 14:15:09 +0200

    Add card component

---
+++ client/src/pages/Card.js
@@ -0,0 +1,17 @@
+ const Card = () => {
+return (
+    <div className = "card-container">
+        <div className = "img-container">
+            {/* <img src ={} alt={}></img> */}
+        </div>
+        <div className = "info-container">
+            <h3>TITLE</h3>
+            <p>DESCRIPTION</p>
+            <div className = "tag-container"></div>
+        </div>
+
+    </div>
+)
+ }
+
+ export default Card
