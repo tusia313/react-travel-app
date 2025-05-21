4a83f766a771b854940524c1731ce074fc97ecbc
Marta Łoskot <loskotmarta@gmail.com>
2025-05-21 14:20:28 +0200

    Gettin all data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,28 @@
 const PORT = 8000
+const { default: axios } = require('axios')
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+//Default
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome to the port 8000 :)')
+})
+//Get all posts
+app.get('/posts', async(req, res) => {
+  const url = `${process.env.ASTRA_URL}?posts-size=20`
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-Cassandra-Token': process.env.TOKEN,
+    }
+  }
+  try {
+    const response = await axios (url, options)
+    res.status(200).json(response.data)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: 'Error fetching posts' })
+  }
+})
 app.listen(PORT, console.log('Server is running on port ' + PORT))
