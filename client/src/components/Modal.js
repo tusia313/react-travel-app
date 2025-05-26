88819855c10f013e37bd6a82be64c0b0d3f62864
Marta Łoskot <loskotmarta@gmail.com>
2025-05-26 15:20:34 +0200

    Create a popup modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,33 @@
+const Modal = () => {
+    const createMode = true
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return(
+        <div className="overlay">
+            <div className = "modal">
+                <form onSubmit={handleSubmit}>
+                    <div className="closing-icon">x</div>
+                        <h1>{createMode ? "Add" : "Edit"} Your adventure</h1>
+                        <h5>Upload a photo of where You have visited</h5>
+                        <p>Paste a url from the internet</p>
+                        <div className="multi-input">
+                            <div className="input-container">
+                                <label for="line">FIRST LINE</label>
+                                <input
+                                id="line"
+                                placeholder="line"
+                                required
+                                name="line"
+                                value={}
+                                onChange = {handleChange}
+                                />
+                            </div>
+                            <div className="input-container"></div>
+                            <div className="input-container"></div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    )
+}
