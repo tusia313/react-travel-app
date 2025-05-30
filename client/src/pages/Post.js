ca040ac28c6cb8af47cca80c1c44173683167ed7
Marta Łoskot <loskotmarta@gmail.com>
2025-05-30 11:15:54 +0200

    Displaying posts in pages

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,6 +1,26 @@
+import { useState } from 'react'
+import Modal from '../components/Modal'
+
 const Post = () => {
+    const [mode, setMode] = useState(null)
     return (
-        <div>POST</div>
+        <div className="post-page">
+            <div className="post-page-container">
+                <div className="info-container">
+                    <div className="title-container">
+                        <h1>xxx</h1>
+                        <h4>xxx</h4>
+                        <p>xxx</p>
+                    </div>
+                    <div className="button-container">
+                        <button>x</button>
+                        <button onClick={()=> setMode('edit')}>✎</button>
+                    </div>
+                </div>
+                {mode && <Modal mode={mode} setMode={setMode} />}
+            </div>
+        </div>
+
     )
 }
 
